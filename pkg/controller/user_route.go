d99be27ab1ec95c3725d69fe455cc5842b6013e2
hong.kim <hong.kim@bstarsolutions.com>
2024-11-27 22:41:16 +0700

    clean code

--- pkg/controller/user_route.go
+++ pkg/controller/user_route.go
@@ -6,7 +6,6 @@ import (
 	"github.com/kimxuanhong/user-manager-go/pkg/api"
 	"github.com/kimxuanhong/user-manager-go/pkg/dto"
 	"github.com/kimxuanhong/user-manager-go/pkg/entity"
-	"log"
 	"sync"
 )
 
@@ -30,15 +29,12 @@ func NewUserRoute(userDao dao.UserDao) UserRoute {
 
 func (r *userRoute) GetUserInfosByPartner(ctx *api.Context, whenDone api.Handler[any]) {
 	partnerId := ctx.Param("id")
-	log.Println("Request " + partnerId)
 	var req dto.Request
 	if err := ctx.Bind(&req); err != nil {
 		whenDone(ctx.Bad(dto.INVALID, err.Error()), nil)
 		return
 	}
 	ctx.SetRequestId(req.RequestId)
-
-	log.Println(req.RequestId)
 	r.userDao.FindUserByPartnerId(ctx, partnerId, func(obj []entity.User, error error) {
 		if error != nil || obj == nil {
 			whenDone(nil, fmt.Errorf("bad request"))
